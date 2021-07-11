91bc66c86ee58fb65188295f7c78679de7b99ead
SanketTambe39 <sankettambe39@gmail.com>
2021-07-12 00:45:17 +0530

    ADDED: First Name validation

--- UserValidation/src/com/bridgelabz/uservalidation/UserValidation.java
+++ UserValidation/src/com/bridgelabz/uservalidation/UserValidation.java
@@ -1,6 +1,25 @@
 package com.bridgelabz.uservalidation;
 
+import java.util.regex.Pattern;
+
 public class UserValidation {
 
-	
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to User Registration");
+		String firstName ="Firstname";
+		checkFirstName(firstName);
+	}
+	private static void checkFirstName(String firstName) 
+	{
+		boolean IsMatched = Pattern.compile("^[A-Z]{1}[a-z]{3,}$").matcher(firstName).matches();
+		if(IsMatched)
+		{
+			System.out.println("First name is correct");
+		}
+		else 
+		{
+			System.out.println("First name is invalid");
+		}
+	}
 }
