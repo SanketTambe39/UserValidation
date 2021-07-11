4ccff7f7a771eb4d48a9b73dadf46b960c551b31
SanketTambe39 <sankettambe39@gmail.com>
2021-07-12 00:55:29 +0530

    ADDED: Email validation

--- UserValidation/src/com/bridgelabz/uservalidation/UserValidation.java
+++ UserValidation/src/com/bridgelabz/uservalidation/UserValidation.java
@@ -1,15 +1,44 @@
 package com.bridgelabz.uservalidation;
 
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserValidation {
 
 	public static void main(String[] args) 
 	{
-		String firstName ="Firstname";
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Welcome to User Registration");
+		
+		//asking first name
+		System.out.println("Enter First name");
+		String firstName =scanner.next();
 		checkFirstName(firstName);
-		String lastName ="Lastname";
+		
+		//asking last name 
+		System.out.println("Enter Last name");
+		String lastName =scanner.next();
 		checkLastName(lastName);
+
+		//asking Email address 
+		System.out.println("Enter Email address");
+		String emailAddress = scanner.next();
+		checkEmailAddress(emailAddress);	
+	
+		scanner.close();
+		
+	}
+	private static void checkEmailAddress(String emailAddress) {
+		boolean isMatched = Pattern.compile("^[0-9a-zA-Z]+([.,+,_,-]{1}[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2})?").matcher(emailAddress).matches();
+		if (isMatched) 
+		{			
+			System.out.println("Email is valid");
+		}
+		else
+		{			
+			System.out.println("Email is invalid");
+		}
+		
 	}
 	private static void checkLastName(String lastName) {
 		boolean IsMatched = Pattern.compile("^[A-Z]{1}[a-z]{3,}$").matcher(lastName).matches();
